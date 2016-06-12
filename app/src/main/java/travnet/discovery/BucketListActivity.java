fc27d0bdbb4563a62ed9a0426a3241e5b8fc36f5
Hassan <hassan.ali.askari@gmail.com>
2016-06-12 20:57:22 +0800

    Modified bucket list UI

--- app/src/main/java/travnet/discovery/BucketListActivity.java
+++ app/src/main/java/travnet/discovery/BucketListActivity.java
@@ -1,6 +1,7 @@
 package travnet.discovery;
 
 import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,10 +10,14 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
@@ -33,14 +38,27 @@ public class BucketListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        userBucketList = new ArrayList<DataBucketListCard>();
+        userBucketList = new ArrayList<>();
         bucketListItemAdapter = new BucketListItemAdapter(this);
 
         //Stub
-        String link = "http://www.planwallpaper.com/static/images/4-Nature-Wallpapers-2014-1_ukaavUI.jpg";
-        DataBucketListCard data1 = new DataBucketListCard(link , "Bali, Indonesia", "Activity");
-        DataBucketListCard data2 = new DataBucketListCard(link, "Lombok, Indonesia", "Activity");
-        DataBucketListCard data3 = new DataBucketListCard(link, "Krabi, Thailand", "Activity");
+
+
+        ArrayList<String> links1 = new ArrayList<>();
+        links1.add("http://science-all.com/images/wallpapers/nature/nature-25.jpg");
+        links1.add("http://www.amaraholisticwellbeing.com/wp-content/uploads/2015/01/Fall-beautiful-nature-22666764-900-562.jpg");
+        DataBucketListCard data1 = new DataBucketListCard("Bali, Indonesia", links1);
+
+        ArrayList<String> links2 = new ArrayList<>();
+        links2.add("https://capfor.files.wordpress.com/2012/07/beautiful-forest-beautiful-day-forests-grass-green-light-nature-sunshine-trees.jpg");
+        DataBucketListCard data2 = new DataBucketListCard("Lombok, Indonesia", links2);
+
+        ArrayList<String> links3 = new ArrayList<>();
+        links3.add("http://hdwpro.com/wp-content/uploads/2015/12/nature-thailand.jpg");
+        links3.add("http://webneel.com/wallpaper/sites/default/files/images/04-2013/mediterranean-beach-wallpaper.jpg");
+        links3.add("https://i.ytimg.com/vi/9Nwn-TZfFUI/maxresdefault.jpg");
+        links3.add("http://tedytravel.com/wp-content/uploads/2016/04/Krabi-8.jpg");
+        DataBucketListCard data3 = new DataBucketListCard("Krabi, Thailand", links3);
         userBucketList.add(data1);
         userBucketList.add(data2);
         userBucketList.add(data3);
@@ -95,25 +113,67 @@ public class BucketListActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
 
-            CardBucketListItemViewHolder cardBucketListItemViewHolder = (CardBucketListItemViewHolder) holder;
+            final CardBucketListItemViewHolder cardBucketListItemViewHolder = (CardBucketListItemViewHolder) holder;
             cardBucketListItemViewHolder.location.setText(userBucketList.get(position).location);
-            ImageLoader.getInstance().displayImage(userBucketList.get(position).imageLink, cardBucketListItemViewHolder.image, options, null);
-
-
+            cardBucketListItemViewHolder.scrollView.setVisibility(View.GONE);
+            populateScrollView(cardBucketListItemViewHolder.locationPictures, userBucketList.get(position).pictures);
+            cardBucketListItemViewHolder.setListener();
         }
     }
 
 
+    private void populateScrollView(LinearLayout layout, List<String> pictures) {
+        for (int i=0; i<pictures.size(); i++) {
+            ImageView imageView = new ImageView(BucketListActivity.this);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(dpToPx(100), dpToPx(100));
+            if (i==0) {
+                params.setMargins(dpToPx(16),0,0,0);
+            } else {
+                params.setMargins(dpToPx(4), 0, 0, 0);
+            }
+            imageView.setLayoutParams(params);
+            layout.addView(imageView);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            ImageLoader.getInstance().displayImage(pictures.get(i), imageView);
+        }
+    }
+
+    private int dpToPx(int dp){
+        final float scale = getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+
     private class CardBucketListItemViewHolder extends RecyclerView.ViewHolder {
-        ImageView image;
+        View cardView;
+        HorizontalScrollView scrollView;
+        LinearLayout locationPictures;
         TextView location;
 
         public CardBucketListItemViewHolder(View itemView) {
             super(itemView);
-            image = (ImageView) itemView.findViewById(R.id.image);
+            cardView = itemView;
+            scrollView = (HorizontalScrollView) itemView.findViewById(R.id.scrollview);
+            locationPictures = (LinearLayout) scrollView.findViewById(R.id.location_pictures);
             location = (TextView) itemView.findViewById(R.id.location);
 
         }
+
+        public void setListener() {
+            this.cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (scrollView.getVisibility() == View.GONE) {
+                        scrollView.setVisibility(View.VISIBLE);
+                    } else {
+                        scrollView.setVisibility(View.GONE);
+                    }
+                }
+            });
+        }
+
+
     }
+
+
 
 }
--- app/src/main/java/travnet/discovery/DataBucketListCard.java
+++ app/src/main/java/travnet/discovery/DataBucketListCard.java
@@ -1,23 +1,25 @@
 package travnet.discovery;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by root on 5/27/16.
  */
 
 public class DataBucketListCard {
 
-    String imageLink;
     String location;
-    String activity;
+    List<String> pictures;
 
     public DataBucketListCard() {
+        pictures = new ArrayList<>();
     }
 
-    public DataBucketListCard(String imageLink, String location, String activity) {
-        this.imageLink = imageLink;
+    public DataBucketListCard(String location, List<String> pictures) {
+        this.pictures = new ArrayList<>();
         this.location = location;
-        this.activity = activity;
-
+        this.pictures.addAll(pictures);
     }
 
 
